bcb3e521388838f54749ee32e2b4148291e84009
kaylaaditya <kaylaaditya123@outlook.com>
2023-11-07 08:15:20 +0700

    perbaiki sidebar

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,11 +1,9 @@
 <form action="?" method="get" class="form-inline">
-<div class="input-group ml-auto">
-  <input type="text" name="search"
-    class="form-control"
-     placeholder="cari..."
-  value="<?= request()->search ?>">
+  <div class="input-group ml-auto">
+    <input type="text" name="search" class="form-control" placeholder="cari..." value="<?= request()->search ?>">
 
-  <div class="input-group-append">
-    <button class="btn btn-outline-secondary" type="submit">Go !</button>
+    <div class="input-group-append">
+      <button class="btn btn-outline-secondary" type="submit">Go !</button>
+    </div>
   </div>
-  </form>
+</form>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,61 +1,127 @@
-
 <!DOCTYPE html>
 
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>{{ isset($title) ? $title.' | '.config('app.name') : config('app.name') }}</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{ isset($title) ? $title.' | '.config('app.name') : config('app.name') }}</title>
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
-<link rel="stylesheet" href="/adminIte/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="/adminIte/plugins/fontawesome-free/css/all.min.css">
 
-<link rel="stylesheet" href="/adminIte/dist/css/adminlte.min.css?v=3.2.0">
-<script nonce="04c245c9-c196-40f1-8dec-f76d1e337ea5">(function(w,d){!function(a,b,c,d){a[c]=a[c]||{};a[c].executed=[];a.zaraz={deferred:[],listeners:[]};a.zaraz.q=[];a.zaraz._f=function(e){return async function(){var f=Array.prototype.slice.call(arguments);a.zaraz.q.push({m:e,a:f})}};for(const g of["track","set","debug"])a.zaraz[g]=a.zaraz._f(g);a.zaraz.init=()=>{var h=b.getElementsByTagName(d)[0],i=b.createElement(d),j=b.getElementsByTagName("title")[0];j&&(a[c].t=b.getElementsByTagName("title")[0].text);a[c].x=Math.random();a[c].w=a.screen.width;a[c].h=a.screen.height;a[c].j=a.innerHeight;a[c].e=a.innerWidth;a[c].l=a.location.href;a[c].r=b.referrer;a[c].k=a.screen.colorDepth;a[c].n=b.characterSet;a[c].o=(new Date).getTimezoneOffset();if(a.dataLayer)for(const n of Object.entries(Object.entries(dataLayer).reduce(((o,p)=>({...o[1],...p[1]})),{})))zaraz.set(n[0],n[1],{scope:"page"});a[c].q=[];for(;a.zaraz.q.length;){const q=a.zaraz.q.shift();a[c].q.push(q)}i.defer=!0;for(const r of[localStorage,sessionStorage])Object.keys(r||{}).filter((t=>t.startsWith("_zaraz_"))).forEach((s=>{try{a[c]["z_"+s.slice(7)]=JSON.parse(r.getItem(s))}catch{a[c]["z_"+s.slice(7)]=r.getItem(s)}}));i.referrerPolicy="origin";i.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a[c])));h.parentNode.insertBefore(i,h)};["complete","interactive"].includes(b.readyState)?zaraz.init():a.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document);</script></head>
+  <link rel="stylesheet" href="/adminIte/dist/css/adminlte.min.css?v=3.2.0">
+  <script nonce="04c245c9-c196-40f1-8dec-f76d1e337ea5">
+    (function(w, d) {
+      ! function(a, b, c, d) {
+        a[c] = a[c] || {};
+        a[c].executed = [];
+        a.zaraz = {
+          deferred: [],
+          listeners: []
+        };
+        a.zaraz.q = [];
+        a.zaraz._f = function(e) {
+          return async function() {
+            var f = Array.prototype.slice.call(arguments);
+            a.zaraz.q.push({
+              m: e,
+              a: f
+            })
+          }
+        };
+        for (const g of ["track", "set", "debug"]) a.zaraz[g] = a.zaraz._f(g);
+        a.zaraz.init = () => {
+          var h = b.getElementsByTagName(d)[0],
+            i = b.createElement(d),
+            j = b.getElementsByTagName("title")[0];
+          j && (a[c].t = b.getElementsByTagName("title")[0].text);
+          a[c].x = Math.random();
+          a[c].w = a.screen.width;
+          a[c].h = a.screen.height;
+          a[c].j = a.innerHeight;
+          a[c].e = a.innerWidth;
+          a[c].l = a.location.href;
+          a[c].r = b.referrer;
+          a[c].k = a.screen.colorDepth;
+          a[c].n = b.characterSet;
+          a[c].o = (new Date).getTimezoneOffset();
+          if (a.dataLayer)
+            for (const n of Object.entries(Object.entries(dataLayer).reduce(((o, p) => ({
+                ...o[1],
+                ...p[1]
+              })), {}))) zaraz.set(n[0], n[1], {
+              scope: "page"
+            });
+          a[c].q = [];
+          for (; a.zaraz.q.length;) {
+            const q = a.zaraz.q.shift();
+            a[c].q.push(q)
+          }
+          i.defer = !0;
+          for (const r of [localStorage, sessionStorage]) Object.keys(r || {}).filter((t => t.startsWith("_zaraz_"))).forEach((s => {
+            try {
+              a[c]["z_" + s.slice(7)] = JSON.parse(r.getItem(s))
+            } catch {
+              a[c]["z_" + s.slice(7)] = r.getItem(s)
+            }
+          }));
+          i.referrerPolicy = "origin";
+          i.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(a[c])));
+          h.parentNode.insertBefore(i, h)
+        };
+        ["complete", "interactive"].includes(b.readyState) ? zaraz.init() : a.addEventListener("DOMContentLoaded", zaraz.init)
+      }(w, d, "zarazData", "script");
+    })(window, document);
+  </script>
+</head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+  <div class="wrapper">
 
-@include('layouts.inc_admin.navbar')
+    @include('layouts.inc_admin.navbar')
 
-@include('layouts.inc_admin.sidebar')
+    @include('layouts.inc_admin.sidebar')
 
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid mb-2">
-       @yield('content-header')
-      </div><!-- /.container-fluid -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid mb-2">
+          @yield('content-header')
+        </div><!-- /.container-fluid -->
+      </div>
+
+
+      <div class="content">
+        <div class="container-fluid">
+          @yield('content')
+        </div>
+      </div>
     </div>
 
-
- <div class="content">
-      <div class="container-fluid">
-       @yield('content')
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+      <div class="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
       </div>
+    </aside>
+
+    <footer class="main-footer">
+
+      <div class="float-right d-none d-sm-inline">
+        version 1.0.1
+      </div>
+
+      <strong>Copyright &copy; 2023 Hafidzul.26</strong> All rights reserved.
+    </footer>
   </div>
 
-   <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-    <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
-    </div>
-  </aside>
-  
-<footer class="main-footer">
 
-<div class="float-right d-none d-sm-inline">
-version 1.0.1
-</div>
-
-<strong>Copyright &copy; 2023 Hafidzul.26</strong> All rights reserved.
-</footer>
-</div>
-
-
-<script src="/adminIte/plugins/jquery/jquery.min.js"></script>
-<script src="/adminIte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="/adminIte/dist/js/.min.js?v=3.2.0"></script>
+  <script src="/adminIte/plugins/jquery/jquery.min.js"></script>
+  <script src="/adminIte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/adminIte/dist/js/.min.js?v=3.2.0"></script>
 </body>
+
 </html>
